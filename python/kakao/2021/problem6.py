319c94258f37d9629db0dc0de13c009d9155c835
daehyun1023 <daehyun1023@naver.com>
2021-03-04 16:20:45 +0900

    sort

---
+++ python/kakao/2021/problem6.py
@@ -0,0 +1,14 @@
+def solution(board, r, c):
+    pass
+
+
+class Pair:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+board = [[1, 0, 0, 3], [2, 0, 0, 0], [0, 0, 0, 2], [3, 0, 1, 0]]
+r = 1
+c = 0
