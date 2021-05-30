23944babcf721f4eed1cb0170138f49d1e41af6e
daehyun1023 <daehyun1023@naver.com>
2021-05-31 02:55:20 +0900

    [20210531] solve

---
+++ java/baekjoon/BOJ16916_부분문자열.java
@@ -0,0 +1,13 @@
+package add;
+import java.util.*;
+public class BOJ16916_부분문자열 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String totalStr = sc.next();
+		String subStr = sc.next();
+		
+		
+	}
+
+}
