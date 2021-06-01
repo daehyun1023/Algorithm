bb4591d2ff8304ded05ec01501bff4ed1f4250dc
daehyun1023 <daehyun1023@naver.com>
2021-06-02 00:02:06 +0900

    [20210602] solve

---
+++ java/baekjoon/BOJ2293동전1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+public class BOJ2293동전1 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[][] dp = new int[N+1][K+1];
+		
+	}
+
+}
